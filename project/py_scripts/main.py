d4e20ba3abedce102c54ebf05ac60280577947e9
TheFileExplorer <kelvin.aung@education.nsw.gov.au>
2025-05-23 12:50:31 +1000

    blink

--- project/py_scripts/main.py
+++ project/py_scripts/main.py
@@ -6,12 +6,15 @@ sleep(0.1)
 
 
 led_pin = 25
-
+led2_pin = 15
 
 led = Pin(led_pin, Pin.OUT)
+led2 = Pin(led2_pin, Pin.OUT)
 
 while True:
    led.value(True) 
-   sleep(1)   
-   led.value(False)  
-   sleep(1)   
+   led2.value(False) 
+   sleep(1)  
+   led.value(False) 
+   led2.value(True)
+   sleep(1)  
