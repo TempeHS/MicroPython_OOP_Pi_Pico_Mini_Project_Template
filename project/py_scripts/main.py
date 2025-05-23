0b462216d78f1692aaf6ed6f55e0b53746a99f35
TheFileExplorer <kelvin.aung@education.nsw.gov.au>
2025-05-23 12:50:40 +1000

    main

--- project/py_scripts/main.py
+++ project/py_scripts/main.py
@@ -7,14 +7,19 @@ sleep(0.1)
 
 led_pin = 25
 led2_pin = 15
+data_pin = 13
 
 led = Pin(led_pin, Pin.OUT)
 led2 = Pin(led2_pin, Pin.OUT)
 
+
+data = Pin(data_pin, Pin.IN)
+
 while True:
-   led.value(True) 
-   led2.value(False) 
-   sleep(1)  
-   led.value(False) 
-   led2.value(True)
-   sleep(1)  
+   if data.value() == 1:
+      led.value(True)
+      led2.value(False)
+   else:
+      led.value(False)
+      led2.value(True)
+   sleep(0.1)
