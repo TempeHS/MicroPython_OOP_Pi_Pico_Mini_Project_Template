2f3d2302b6238230ca0fe1b2c1f28f18d8e378fa
RyanCai3 <ryan.cai3@education.nsw.gov.au>
2025-05-23 12:51:48 +1000

    Bruh Again

--- project/py_scripts/1.blink_led.py
+++ project/py_scripts/1.blink_led.py
@@ -4,11 +4,15 @@ from time import sleep
 sleep(0.1)
 
 led_pin = 25
+led2_pin = 15
 
-led = Pin(led_pin, pin_OUT)
+led = Pin(led_pin, Pin.OUT)
+led2 = Pin(led2_pin, Pin.OUT)
 
 while True:
     led.value(True)
-    time.sleep(1)
+    led2.value(False)
+    sleep(1)
     led.value(False)
-    time.sleep(1)
+    led2.value(True)
+    sleep(1)
