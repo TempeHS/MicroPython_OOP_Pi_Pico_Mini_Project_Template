5f959836381e56d07563542d7f696b136fd4c533
tk <4tkbytes@pm.me>
2025-06-13 10:32:43 +1000

    my keyboard broke :(

--- project/py_scripts/fuckall_unittest.py
+++ project/py_scripts/fuckall_unittest.py
@@ -1,5 +1,5 @@
 import fuckall
-from time import sleep
+from time import sleep, time
 
 def test_led():
     # firstly get it to initialise
@@ -54,8 +54,40 @@ def test_led():
 
 def test_button():
     
+    # Replace 22 with the GPIO pin your button is connected to
+    button = Pedestrian_Button(22, debug=True)
+
+    print("Testing initial button_state (should be False if not pressed)")
+    initial_state = button.button_state
+    if initial_state is False:
+        print("Initial .button_state passed")
+    else:
+        print("Initial .button_state failed")
+
+    print("Please press and release the button within 5 seconds...")
+    pressed = False
+    for _ in range(50):
+        if button.button_state:
+            pressed = True
+            break
+        sleep(0.1)
+
+    if pressed:
+        print("Button press detected: .button_state passed")
+    else:
+        print("Button press not detected: .button_state failed")
+
+    print("Testing button_state setter (reset to False)")
+    button.button_state = False
+    sleep(0.1)
+    if button.button_state is False:
+        print(".button_state setter passed")
+    else:
+        print(".button_state setter failed")
+    
     print("-------------------------------------------")
     print("Button has finished testing, all success")
     print("-------------------------------------------")
 
-test_led()
+# test_led()
+test_button()
